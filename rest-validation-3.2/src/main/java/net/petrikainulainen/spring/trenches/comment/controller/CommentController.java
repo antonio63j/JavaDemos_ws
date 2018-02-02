3823c3ea7f564eb7117f9aa1f9eb823682f8bf90
antonio63j <antonio63j@gmail.com>
2018-02-02 13:33:39 +0100

    otros

---
+++ rest-validation-3.2/src/main/java/net/petrikainulainen/spring/trenches/comment/controller/CommentController.java
@@ -0,0 +1,25 @@
+package net.petrikainulainen.spring.trenches.comment.controller;
+
+import net.petrikainulainen.spring.trenches.comment.dto.CommentDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @author Petri Kainulainen
+ */
+@Controller
+public class CommentController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommentController.class);
+
+    @RequestMapping(value = "/api/comment", method = RequestMethod.POST)
+    @ResponseBody
+    public CommentDTO add(@Valid @RequestBody CommentDTO comment) {
+        LOGGER.debug("Received __ comment: {}", comment);
+        return comment;
+    }
+}
